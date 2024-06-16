6e594f97a9e8971324f1427f354764ffd4eaef23
James <jamesa2600@gmail.com>
2024-06-16 11:49:07 -0700

    updates to themes

---
+++ dwm/themes/gruvbox.h
@@ -0,0 +1,15 @@
+static const char black[]       = "#282828";
+static const char red[]         = "#cc241d";
+static const char green[]       = "#98971a";
+static const char yellow[]      = "#d79921";
+static const char blue[]        = "#458588";  // focused window border
+static const char purple[]      = "#b16286";
+static const char aqua[]        = "#8ec07c";
+static const char gray2[]       = "#504945"; // unfocused window border
+static const char gray3[]       = "#665c54";
+static const char gray4[]       = "#7c6f64";
+static const char white[]       = "#ebdbb2";
+static const char orange[]      = "#d65d0e";
+static const char pink[]        = "#b16286";  //remove this duplicte
+
+static const char col_borderbar[]  = "#282828"; // border for title bar dont change
